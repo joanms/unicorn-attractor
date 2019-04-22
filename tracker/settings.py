787e219f31f7a794c161c619a41f77aecd4a84b8
Joan Sweeney <joanms@eircom.net>
2019-04-22 13:05:27 +0000

    Start adding email functionality to password reset

--- tracker/settings.py
+++ tracker/settings.py
@@ -137,4 +137,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587

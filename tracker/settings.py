53931aab87cd4efe2d53f9b2a5a81a95c69d39f1
Joan Sweeney <joanms@eircom.net>
2019-04-23 16:03:25 +0000

    Replace gmail email settings with SendGrid

--- tracker/settings.py
+++ tracker/settings.py
@@ -139,7 +139,7 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_PORT = 587

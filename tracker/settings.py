2c85f34df7a08b82b3aa673825934fad5524c426
Joan Sweeney <joanms@eircom.net>
2019-04-24 19:59:03 +0000

    Put secret key in environment variable

--- tracker/settings.py
+++ tracker/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_1p(+g#w5_$0qcodf*x1yq*uayjh&a=qwzd(v4frq935t6-xyx'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

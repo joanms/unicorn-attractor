15476294b34c3f85cce345885365e21f693b87ef
Joan Sweeney <joanms@eircom.net>
2019-04-24 15:10:26 +0000

    Add tests for login form

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,7 +1,19 @@
 from django.test import TestCase
+from .forms import UserLoginForm
 
 # Create your tests here.
-class TestDjango(TestCase):
+class LoginForm(TestCase):
     
-    def test_is_this_thing_on(self):
-        self.assertEqual(1, 1)
+    def test_can_log_in(self):
+        form = UserLoginForm({'username': 'FooBar', 'password': 'abc123!?'})
+        self.assertTrue(form.is_valid)
+        
+    def test_username_is_required(self):
+        form = UserLoginForm({'username': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['username'], [u'This field is required.'])
+        
+    def test_password_is_required(self):
+        form = UserLoginForm({'password': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['password'], [u'This field is required.'])        

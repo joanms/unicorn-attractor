c98d0d5b9244c69c24992376b313abaae192f6f9
Joan Sweeney <joanms@eircom.net>
2019-04-24 16:26:08 +0000

    Added tests for registration form

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegistrationForm
 
 # Create your tests here.
 class LoginForm(TestCase):
@@ -16,4 +16,25 @@ class LoginForm(TestCase):
     def test_password_is_required(self):
         form = UserLoginForm({'password': ''})
         self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors['password'], [u'This field is required.'])        
+        self.assertEqual(form.errors['password'], [u'This field is required.'])
+        
+class RegistrationForm(TestCase):
+    
+    def test_can_register(self):
+        form = UserRegistrationForm({'email': 'test@example.com', 'username': 'FooBar', 'password1': 'abc123!?', 'password2': 'abc123!?'})
+        self.assertTrue(form.is_valid)
+
+    def test_email_is_required(self):
+        form = UserRegistrationForm({'email': ''})
+        self.assertFalse(form.is_valid())
+        # Need to add error message - fix problem with key error
+        
+    def test_username_is_required(self):
+        form = UserRegistrationForm({'username': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['username'], [u'This field is required.'])
+
+    def test_passwords_must_match(self):
+        form = UserRegistrationForm({'password1': 'abc123!?', 'password2': 'xyz123!?'})
+        self.assertFalse(form.is_valid())
+

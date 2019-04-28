088dc861646f12a8051b81e1da5dc1274f994fb8
Joan Sweeney <joanms@eircom.net>
2019-04-28 16:31:43 +0000

    Add view, template and URL pattern for bug reporting

--- bugs/views.py
+++ bugs/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def report_bug(request):
+    return render(request, "report_bug.html") 
--- tracker/urls.py
+++ tracker/urls.py
@@ -19,10 +19,12 @@ from home import urls as urls_home
 from accounts import urls as urls_accounts
 from django.views.static import serve
 from .settings import MEDIA_ROOT
+from bugs.views import report_bug
 
 urlpatterns = [
     url(r'^', include(urls_home)),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT }),
+    url(r'^report_bug/', report_bug),
 ]

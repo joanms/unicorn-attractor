5902f5d6f46f1d0f50edad218acc3c442f400764
Joan Sweeney <joanms@eircom.net>
2019-04-29 10:24:22 +0000

    Add bug submission form

--- bugs/forms.py
+++ bugs/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 from .models import Bug
 
+
 class BugReportForm(forms.ModelForm):
     
     """Form for users to report bugs"""
     
     class Meta:
         model = Bug
-        fields = ('title', 'description')
+        fields = ['title', 'description']
+        
+    title = forms.CharField()
+    description = forms.Textarea()
--- bugs/views.py
+++ bugs/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, redirect, reverse
+from bugs.forms import BugReportForm
 
 # Create your views here.
 def report_bug(request):
-    return render(request, 'report_bug.html')
+    bug_form = BugReportForm(request.POST or None)
+    if bug_form.is_valid():
+        bug_form.save()
+        
+    context = {
+        'bug_form': bug_form
+    }    
+        
+    return render(request, 'report_bug.html', context)

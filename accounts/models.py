fcaee4808963a7de181cff6558762934f2ce0775
Joan Sweeney <joanms@eircom.net>
2019-05-07 14:48:37 +0000

    Reinsert code to create a user profile when a user is created

--- accounts/models.py
+++ accounts/models.py
@@ -20,3 +20,14 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.user.username
+        
+# The following code is from https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+        

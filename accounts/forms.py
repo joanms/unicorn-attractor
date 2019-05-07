629998f60279350bc243d397e44037b7e11d9911
Joan Sweeney <joanms@eircom.net>
2019-05-07 14:27:14 +0000

    Reset migrations to fix bug in admin panel

--- accounts/forms.py
+++ accounts/forms.py
@@ -26,7 +26,7 @@ class UserRegistrationForm(UserCreationForm):
         
     class Meta:
         model = User
-        fields = ['email', 'username', 'password1', 'password2',]
+        fields = ['email', 'username', 'password1', 'password2']
         
     def clean_email(self):
         email = self.cleaned_data.get('email')

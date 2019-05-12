70428ad1433d5ae2336ec76f30478c99b504ce27
Joan Sweeney <joanms@eircom.net>
2019-05-12 10:35:26 +0000

    Add automated tests for bugs app

--- bugs/tests.py
+++ bugs/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from .forms import BugReportForm, CommentForm
 
-# Create your tests here.
+
+class TestForms(TestCase):
+
+    def test_can_report_bug(self):
+        
+        """Ensure that a user can report a bug"""
+
+        form = BugReportForm({'title': 'Unicorns', 'description': 'Too many unicorns.'})
+        self.assertTrue(form.is_valid)
+
+    def test_can_comment_on_a_bug(self):
+        
+        """Ensure that a user can report a bug"""
+
+        form = CommentForm({'text': 'I like the unicorns.'})
+        self.assertTrue(form.is_valid)
+        
+        
+class TestViews(TestCase):
+
+    def test_get_bug_list_page(self):
+    
+        """Ensure that the bug list page loads correctly"""
+    
+        page = self.client.get("/bugs/view_bugs/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "view_bugs.html")
+
+        
+    # When I run the following test, it fails with this message: 'AssertionError: 302 != 200'
+    # This means that the requested resource resides temporarily under a different URI.
+    # The page always loads correctly when I test it manually.
+    def test_get_bug_report_page(self):
+    
+        """Ensure that the bug report form page loads correctly"""
+    
+        page = self.client.get("/bugs/report_bug/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "report_bug.html")                

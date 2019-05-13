d4b2db26161b8682707e55b608ce2a04bb2a96ae
Joan Sweeney <joanms@eircom.net>
2019-05-13 16:16:19 +0000

    Adjust linebreaks in index.html and add test for home app

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestViews(TestCase):
+    
+    def test_get_index_page(self):
+    
+        """Ensure that the index page loads correctly"""
+    
+        page = self.client.get("/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "index.html")

2d1fbe535311f6163ee61aa9edee78195499bee6
Joan Sweeney <joanms@eircom.net>
2019-05-14 16:30:30 +0000

    Fix errors in filenames and URLs

--- features/urls.py
+++ features/urls.py
@@ -4,7 +4,7 @@ from .views import request_feature, feature_details, list_features, upvote_featu
 urlpatterns = [
     url(r'^list_features/$', list_features, name="list_features"),
     url(r'^(?P<pk>\d+)/$', feature_details, name="feature_details"),
-    url(r'^report_feature/$', request_feature, name="request_feature"),
+    url(r'^request_feature/$', request_feature, name="request_feature"),
     # The code for upvoting is based on this: https://www.quora.com/How-do-I-create-a-vote-button-in-Django/answer/Lakshmi-Suvvada
     url(r'^upvote/(?P<feature_id>[0-9]+)/$', upvote_feature, name='upvote_feature'),
     url(r'^comment/(?P<pk>\d+)/$', feature_comment, name="feature_comment"),
--- tracker/settings.py
+++ tracker/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django_forms_bootstrap',
     'accounts',
     'bugs',
-    'cart',
     'checkout',
     'features',
     'graphs',

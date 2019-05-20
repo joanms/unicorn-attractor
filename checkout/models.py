ff21d491d9d814020fffa862d877db7ba6b357d7
Joan Sweeney <joanms@eircom.net>
2019-05-20 10:17:24 +0000

    Make upvoter in Order model nullable

--- checkout/models.py
+++ checkout/models.py
@@ -15,7 +15,7 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=40, blank=True)
     county = models.CharField(max_length=40, blank=False)
     date = models.DateField()
-    upvoter = models.ForeignKey(User, default="admin", on_delete=models.CASCADE)
+    upvoter = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)

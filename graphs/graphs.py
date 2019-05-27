34308ec80bccd012de830e8ed593ab2f0fd265d4
Joan Sweeney <joanms@eircom.net>
2019-05-27 15:42:13 +0000

    Add graphs for common bugs and popular features

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -2,6 +2,7 @@ import pygal
 from bugs.models import Bug
 from features.models import Feature
 from pygal.style import Style
+from django.core import serializers
 
 custom_style = Style(
     background = 'transparent',
@@ -11,7 +12,7 @@ custom_style = Style(
     value_font_size=25)
 
 def bug_pie_chart():
-    """Show summary for features"""
+    """Show status of bugs"""
 
     todo = Bug.objects.filter(status='To do').count()
     inprogress = Bug.objects.filter(status='In progress').count()
@@ -33,7 +34,7 @@ def BugPieChart():
     return chart
     
 def feature_pie_chart():
-    """Show summary for features"""
+    """Show status of features"""
 
     todo = Feature.objects.filter(status='To do').count()
     inprogress = Feature.objects.filter(status='In progress').count()
@@ -53,3 +54,42 @@ def feature_pie_chart():
 def FeaturePieChart():
     chart = feature_pie_chart()
     return chart    
+    
+    
+def bug_bar_chart():
+    """Show commonness of bugs"""
+
+    bugs = Bug.objects.all()
+    line_chart = pygal.HorizontalBar(
+        print_values=True, 
+        style=custom_style
+    )
+
+    for bug in bugs:
+        line_chart.add(bug.title, bug.upvotes)
+
+    return line_chart.render()
+
+def BugBarChart():
+    chart = bug_bar_chart()
+    return chart
+
+
+def feature_bar_chart():
+    """Show popularity of features"""
+
+    features = Feature.objects.all()
+    line_chart = pygal.HorizontalBar(
+        print_values=True, 
+        style=custom_style
+    )
+
+    for feature in features:
+        line_chart.add(feature.title, feature.amount_paid)
+
+    return line_chart.render()
+
+def FeatureBarChart():
+    chart = feature_bar_chart()
+    return chart
+    
--- graphs/views.py
+++ graphs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .graphs import BugPieChart, FeaturePieChart
+from .graphs import BugPieChart, FeaturePieChart, BugBarChart, FeatureBarChart
 
 def graphs(request):
     """A view that displays the graphs page"""
-    return render(request, "graphs.html", {'bug_pie_chart': BugPieChart(), 'feature_pie_chart': FeaturePieChart()})
+    return render(request, "graphs.html", {'bug_pie_chart': BugPieChart(), 'feature_pie_chart': FeaturePieChart(), 'bug_bar_chart': BugBarChart(), 'feature_bar_chart': FeatureBarChart()})

9b42c73beb243e3c9a34d0e4ae6c6e4e3ef40b1e
Joan Sweeney <joanms@eircom.net>
2019-06-05 11:15:09 +0000

    Replace euro symbol with unicode glyph number in cart/views.py and checkout/models.py

--- cart/views.py
+++ cart/views.py
@@ -65,7 +65,7 @@ def subtract_one(request, id):
     if cart[id] > 5:
         cart[id] = cart[id] - 1
     else:
-        messages.error(request, "The minimum price is €5. To remove the item from the cart, please click Delete.")
+        messages.error(request, "The minimum price is \u20ac5. To remove the item from the cart, please click Delete.")
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))

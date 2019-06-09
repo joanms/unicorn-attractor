4cabe79db3a7b077239a60132829607c341b6c8d
Joan Sweeney <joanms@eircom.net>
2019-06-09 13:58:53 +0000

    Move JavaScript for graphs from graphs.html to custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,28 +1,64 @@
-$(document).ready(function(){
-    
-    $('#plus-one').click(function() {
-        $.get('/cart/add_one/<id>').done(function(response) {
-            // result goes here
-            console.log(response);
-            // response = {
-            //     'new_price': '6'
-            // }
-            
-            // $('#price').text(response.new_price);
-        });
-    });
+new Chart(document.getElementById("bug-upvotes"), {
+    type: 'horizontalBar',
+        
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            backgroundColor: '#583870',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
 
-    $('#minus-one').click(function() {
-        $.get('/cart/subtract_one/<id>').done(function(response) {
-            // result goes here
-            console.log(response);
-            // response = {
-            //     'new_price': '6'
-            // }
-            
-            // $('#price').text(response.new_price);
-        });
-    });
-
+    options: {
+        legend: { display: false }
+    }
+});
     
-})
+new Chart(document.getElementById("feature-upvotes"), {
+    type: 'horizontalBar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            backgroundColor: '#583870',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+        
+    options: {
+        legend: { display: false }
+    }
+});
+        
+new Chart(document.getElementById("bug-status"), {
+    type: 'pie',
+    data: {
+      labels: ["To Do", "In Progress", "Done", "Cancelled"],
+      datasets: [{
+        backgroundColor: ["#2E2FE3", "#583870","#AE0D7A","#5D1D3A"],
+        borderColor: "#D4D4F7",
+        data: [2478,5267,734,784]
+      }]
+    },
+    options: {
+      title: {
+        display: false
+      }
+    }
+});
+        
+new Chart(document.getElementById("feature-status"), {
+    type: 'pie',
+    data: {
+      labels: ["To Do", "In Progress", "Done", "Cancelled"],
+      datasets: [{
+        backgroundColor: ["#2E2FE3", "#583870","#AE0D7A","#5D1D3A"],
+        borderColor: "#D4D4F7",
+        data: [2478,5267,734,784]
+      }]
+    },
+    options: {
+      title: {
+        display: false
+      }
+    }
+});

f0c0f44065259e926faaec7ae6650af260f3841b
Joan Sweeney <joanms@eircom.net>
2019-07-03 13:32:47 +0100

    Start adding Ajax code to get chart data

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -1,64 +1,96 @@
-new Chart(document.getElementById("bug-upvotes"), {
-    type: 'horizontalBar',
-        
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            backgroundColor: '#700CBC',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
+// The Ajax code is based on Anthony Bonello's project: https://github.com/abonello/project_5/blob/master/static/js/my_charts.js
 
-    options: {
-        legend: { display: false }
-    }
-});
-    
-new Chart(document.getElementById("feature-upvotes"), {
-    type: 'horizontalBar',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+$.ajax({
+  type: "GET",
+  url: "get_chart_data",
+  dataType: "json",
+  astnc: true,
+
+  success: function (data) {
+    var bug_titles = [];
+    var bug_upvotes = [];
+    var feature_titles = [];
+    var feature_upvotes = [];
+    var bug_status = [];
+    var bug_quantity = [];
+    var feature_status = [];
+    var feature_quantity = [];
+
+    $.each(data.message.bugs, function (i, content) {
+      bug_titles.push(content.title);
+      bug_upvotes.push(content.votes);
+      bug_status.push(content.comments);
+      bug_quantity.push(content.comments);
+    });
+
+    $.each(data.message.features, function (i, content) {
+      feature_titles.push(content.title);
+      feature_upvotes.push(content.votes);
+      feature_status.push(content.comments);
+      feature_quantity.push(content.comments);
+    });
+
+    new Chart(document.getElementById("bug-upvotes"), {
+      type: 'horizontalBar',
+
+      data: {
+        labels: bug_titles,
         datasets: [{
-            backgroundColor: '#700CBC',
-            data: [0, 10, 5, 2, 20, 30, 45]
+          backgroundColor: '#700CBC',
+          data: bug_upvotes
         }]
-    },
-        
-    options: {
+      },
+
+      options: {
         legend: { display: false }
-    }
-});
-        
-new Chart(document.getElementById("bug-status"), {
-    type: 'pie',
-    data: {
-      labels: ["To Do", "In Progress", "Done", "Cancelled"],
-      datasets: [{
-        backgroundColor: ["#2E2FE3", "#700CBC","#AE0D7A","#5D1D3A"],
-        borderColor: "#D4D4F7",
-        data: [2478,5267,734,784]
-      }]
-    },
-    options: {
-      title: {
-        display: false
       }
-    }
-});
-        
-new Chart(document.getElementById("feature-status"), {
-    type: 'pie',
-    data: {
-      labels: ["To Do", "In Progress", "Done", "Cancelled"],
-      datasets: [{
-        backgroundColor: ["#2E2FE3", "#700CBC","#AE0D7A","#5D1D3A"],
-        borderColor: "#D4D4F7",
-        data: [2478,5267,734,784]
-      }]
-    },
-    options: {
-      title: {
-        display: false
+    });
+
+    new Chart(document.getElementById("feature-upvotes"), {
+      type: 'horizontalBar',
+      data: {
+        labels: feature_titles,
+        datasets: [{
+          backgroundColor: '#700CBC',
+          data: feature_upvotes
+        }]
+      },
+
+      options: {
+        legend: { display: false }
       }
-    }
-});
+    });
+
+    new Chart(document.getElementById("bug-status"), {
+      type: 'pie',
+      data: {
+        labels: bug_status,
+        datasets: [{
+          backgroundColor: ["#2E2FE3", "#700CBC", "#AE0D7A", "#5D1D3A"],
+          borderColor: "#D4D4F7",
+          data: bug_quantity
+        }]
+      },
+      options: {
+        title: {
+          display: false
+        }
+      }
+    });
+
+    new Chart(document.getElementById("feature-status"), {
+      type: 'pie',
+      data: {
+        labels: feature_status,
+        datasets: [{
+          backgroundColor: ["#2E2FE3", "#700CBC", "#AE0D7A", "#5D1D3A"],
+          borderColor: "#D4D4F7",
+          data: feature_quantity
+        }]
+      },
+      options: {
+        title: {
+          display: false
+        }
+      }
+    });
